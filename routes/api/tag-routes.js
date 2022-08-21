89258adf565c9910bec0d7619e6a4079763432c0
dmadon <deanna.madon@gmail.com>
2022-08-21 15:42:55 -0500

    successfully added route to delete a tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -80,8 +80,24 @@ router.put('/:id', (req, res) => {
   })
 });
 
+// delete a tag by id
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if(!dbTagData){
+      res.status(400).json({message: "No tag found with that id."});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
